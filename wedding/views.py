6d05f56ca63e2495fe108c76f8329d7eedeb36ed
Kyle Marsh <kyle.marsh@dreamhost.com>
2013-07-24 10:42:04 -0700

    Alertify error messages
    Use alertify.js to show the error/success messages for RSVPs.

--- wedding/views.py
+++ wedding/views.py
@@ -23,55 +23,46 @@ def rsvp():
                 party = parties[0]
                 parties = None
                 # populate form
+            else:
+                flash("Couldn't find a party for '%s'." % query, 'error')
 
     elif request.method == 'POST':
         try:
             party = Party.query.get(form['party_id'])
             if not party:
-                flash('Please try again.')
+                flash('Please try again.', 'error')
                 raise ValidationError('no party')
 
             for attendee in party.attendees:
                 a_id = attendee.attendee_id
                 attending = ('%s_attending' % a_id) in form
-                print "Are we attending? %s_attending: %s" % (a_id, attending)
                 if attending:
-                    print "I'm ATTENDING. LETS VALIDATE"
                     # Let's get validating.
                     name = form['%s_name' % a_id]
                     if name == 'Plus One' or not name:
-                        flash("Give your plus one a name!")
+                        flash("Give your plus one a name!", 'error')
                         raise ValidationError()
                     if attendee.name and name != attendee.name:
-                        flash("Give your plus one a name!")
+                        flash("Give your plus one a name!", 'error')
                         raise ValidationError()
                     attendee.name = name
 
                     meal = form['%s_meal' % a_id]
-                    print "meal is %s" % meal
                     if meal not in ['0', '1']:
-                        flash("Pick meals from the drop-down")
+                        flash("Pick meals from the drop-down", 'error')
                         raise ValidationError()
                     attendee.meal_choice = meal
                     attendee.attending = True
                 else:
                     # They're not coming. Don't validate anything and just
                     # turn them off.
-                    print "I'm NOT ATTENDING."
                     attendee.attending = False
-            print "no validation errors. Saving..."
             db.session.commit()
-            print "saved. flashing..."
-            flash("Thanks! Your reservation has been updated")
-            print "done. redirecting to ceremony."
+            flash("Thanks! Your reservation has been updated", 'success')
             return redirect(url_for('index', _anchor='ceremony'))
 
-            print "wat?"
         except ValidationError:
-            print "got a validation error"
             return redirect(url_for('rsvp'))
-
-        print "wat."
 
     return render_template('rsvp.html', query=query, parties=parties,
             party=party)
